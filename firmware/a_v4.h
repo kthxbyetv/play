585f49638ef087021e27a3b2def23d588505ca4e
TiBounise <contact@tibounise.com>
2014-05-21 07:33:47 +0200

    Removing unexisting function prototypes

--- firmware/a_v4.h
+++ firmware/a_v4.h
@@ -18,8 +18,5 @@ struct IrCode {
 };
 
 void xmitCodeElement(uint16_t ontime, uint16_t offtime, uint8_t PWM_code );
-void flashslowLEDx( uint8_t num_blinks );
-void quickflashLEDx( uint8_t x );
 void tvbgone_sleep( void );
 void delay_ten_us(uint16_t us);
-void quickflashLED( void );
