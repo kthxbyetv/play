31942bc38eca60d6e9f344468e518c2229710bdb
Alexis Brouste <linuxomaniac@Nyaruko>
2014-05-31 10:27:32 +0200

    Ajout de la fonction pour que lorsqu'on appuie à nouveau sur le bouton lors du processus, cela stoppe la diffusion des codes. En gros, le bouton fait maintenant ON/OFF au lieu de faire seulement ON.

--- firmware/main.c
+++ firmware/main.c
@@ -23,48 +23,45 @@ distributed under Creative Commons 2.5 -- Attib & Share Alike
 extern const PGM_P * const EUpowerCodes[] PROGMEM;
 extern const uint8_t num_EUcodes;
 
-uint8_t bitsleft_r = 0;
-uint8_t bits_r=0,grenade=0;
+uint8_t bitsleft_r = 0, bits_r = 0;
 PGM_P code_ptr;
+volatile uint8_t working = 0;
 
 uint8_t read_bits(uint8_t count) {
 	uint8_t i;
 	uint8_t tmp=0;
-  
+
 	// we need to read back count bytes
 	for (i=0; i<count; i++) {
 		// check if the 8-bit buffer we have has run out
 		if (bitsleft_r == 0) {
 			// in which case we read a new byte in
 			bits_r = pgm_read_byte(code_ptr++);
-	
+
 			// and reset the buffer size (8 bites in a byte)
 			bitsleft_r = 8;
 		}
-		
+
 		// remove one bit
 		bitsleft_r--;
-		
+
 		// and shift it off of the end of 'bits_r'
 		tmp |= (((bits_r >> (bitsleft_r)) & 1) << (count-1-i));
 	}
-	
+
 	// return the selected bits in the LSB part of tmp
-	return tmp; 
+	return tmp;
 }
 
 ISR(INT0_vect) {
-	// To reset the AVR without relying on hardware,
-	// we enable the watchdog then launch an endless loop
-	// so that the watchdog will detect it and reset the AVR
-	wdt_enable(WDTO_15MS);
-	for(;;) {}
+	// Set working to 0, so it will break the loop and make the chip enter in sleep mode
+	working = 0;
 }
 
 int main(void) {
 	// Variables initialisation
 	uint16_t ontime, offtime;
-	uint8_t i,loopgrn;
+	uint8_t i;
 
 	// Clearing watchdogs flags and disabling it
 	MCUSR = 0;
@@ -88,8 +85,14 @@ int main(void) {
 	// Turn on interrupts
 	sei();
 
-	for (;;) {
+	for(;;) {
+		// We start the work, so let's define the variable
+		working = 1;
 		for(i=0 ; i < num_EUcodes; i++) {
+			// Check the interrupt
+			if(working == 0)
+				break;
+
 			// Reset our watchdog timer, so it won't reset the AVR while sending a code
 			wdt_reset();
 
@@ -98,9 +101,9 @@ int main(void) {
 
 			// Read the carrier frequency
 			const uint8_t freq = pgm_read_byte(code_ptr++);
-				
+
 			// set OCR for Timer1 to output this code's carrier frequency
-			OCR0A = freq; 
+			OCR0A = freq;
 
 			// Get the number of pairs
 			const uint8_t numpairs = pgm_read_byte(code_ptr++);
@@ -125,43 +128,55 @@ int main(void) {
 				offtime = pgm_read_word(time_ptr+ti+2);  // read word 2 - offtime
 
 				// transmit this codeElement (ontime and offtime)
-				xmitCodeElement(ontime, offtime, (freq!=0));  
+				xmitCodeElement(ontime, offtime, (freq!=0));
 			}
 
 			//Flush remaining bits, so that next code starts
 			//with a fresh set of 8 bits.
-			bitsleft_r=0;	
+			bitsleft_r = 0;
 
 			// delay 250 milliseconds before transmitting next POWER code
-			delay_ten_us(25000);
+			delay_ten_us(25000);// TODO : decrease this number to accelerate the process ?
 
 			// visible indication that a code has been output.
-			blinkLED(1); 
-		}  
+			blinkLED(1);
+		}
+		// Done
+		working = 0;
 
 		// We are done, no need for a watchdog timer anymore
 		wdt_disable();
 
-		// flash the visible LED on PB0  4 times to indicate that we're done
+		// flash the visible LED on PB0 4 times to indicate that we're done
 		blinkLED(10);
 
 		// Shut down the timer
 		TCCR0A = 0;
 
+		/* CHECK : Is this really necessary ?
 		// Setting the pullup on the button
-		PORTB = 0b00000100;
+		// PORTB = _BV(BUTTON);
+		*/
 
 		// Make sure our watchdog is disabled
 		wdt_disable();
 
+		/* CHECK : This too ?
 		// Set an interrupt on INT0 (which is PB2)
-		GIMSK = _BV(INT0);
+		// GIMSK = _BV(INT0);
+		*/
 
 		// Put the CPU into sleep mode
-		MCUCR = _BV(SM1) |  _BV(SE);
+		MCUCR = _BV(SM1) | _BV(SE);
 		sleep_cpu();
 
+		// A little delay, otherwise it won't work and the CPU will return in sleep mode immediatly
+		delay_ten_us(6000);
+
 		// Reset the AVR when it wakes up
+		// To reset the AVR without relying on hardware,
+		// we enable the watchdog then launch an endless loop
+		// so that the watchdog will detect it and reset the AVR
 		wdt_enable(WDTO_15MS);
 		for(;;) {}
 	}
